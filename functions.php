d0b9f672ce063161ee4a98d9fbd97d90fe75bbc0
Bo Zhang <bzhang3@mail.greenriver.edu>
2019-06-11 10:22:50 -0400

    Added function distribution.

--- functions.php
+++ functions.php
@@ -19,3 +19,22 @@
     {
         return array_unique($numbers);
     }
+
+    // defines function
+    function distribution($numbers)
+    {
+        $newArray = array();
+
+        foreach($numbers as $number)
+        {
+            if (array_key_exists($number, $newArray)) {
+                $value = $newArray[$number];
+                $value = $value + 1;
+                $newArray[$number] = $value;
+            } else {
+                $newArray[$number] = 1;
+            }
+        }
+
+        return $newArray;
+    }
