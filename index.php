ed74d433a0b1d1f6b83c46430d4645318d6776a4
Bo Zhang <bzhang3@mail.greenriver.edu>
2019-06-11 01:11:25 -0400

    Added a block comment, error reporting, and title.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    /*
+     * Name: Bo Zhang
+     * Date: 6/10/2019
+     * File URL: http://bzhang.greenriverdev.com/328/pp1/
+     */
+
+    // turns on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pair Program 1</title>
+    </head>
+
+    <body>
+
+    </body>
+</html>
+
